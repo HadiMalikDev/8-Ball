610b9c516f7c39c468f330e1cab1ec9397ba7fd9
MHadi-Dev <Hadimalik1027@gmail.com>
2021-02-03 10:39:07 +0500

    First upload

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,51 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
+void main() {
+  runApp(
+    BallApp(),
+  );
+}
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+class BallApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Ask me anything"),
+          centerTitle: true,
+          backgroundColor: Color(0xFF00008B),
+        ),
+        body: BallAppBody(),
+        backgroundColor: Colors.lightBlue,
       ),
     );
+  }
+}
+
+class BallAppBody extends StatefulWidget {
+  BallAppBody({Key key}) : super(key: key);
+
+  @override
+  _BallAppBodyState createState() => _BallAppBodyState();
+}
+
+class _BallAppBodyState extends State<BallAppBody> {
+  @override
+  int randVal = 1;
+  Widget build(BuildContext context) {
+    return Center(
+      child: GestureDetector(
+        child: Image.asset('images/ball$randVal.png'),
+        onTap:() {
+          setState((){
+            randVal=Random().nextInt(5)+1;
+            }
+          );
+
+        },
+        ),
+    );
+  }
+}
